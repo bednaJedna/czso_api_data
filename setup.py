069de17bc86f96ab07c70ee27f0a2638130c22fd
bednaJedna <bednarik.radek@gmail.com>
2020-03-08 19:49:53 +0100

    docstrings added

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(packages=find_packages(), install_requires=["xlsxwriter", "pandas"])
+setup(packages=find_packages(), install_requires=["xlsxwriter", "pandas", "pyre-check"])
--- src/api/api.py
+++ src/api/api.py
@@ -53,6 +53,27 @@ class API:
     def _get_all(
         self, api_suffix: Text, skip_start=0, skip_step=30, sleep_=False
     ) -> Any:
+        """Private method.
+        Gets all data from given resource of the api, as specified via api_suffix
+        argument.
+        Data are stored as pandas dataframe in public self.data attribute of the API class instance.
+        Api data are provided as 30 records per "page", i.e.
+        list of 30 dicts.
+        
+        Arguments:
+            api_suffix {Text} -- resource for specific data which is added to base api url
+        
+        Keyword Arguments:
+            skip_start {int} -- offset, where to start with data retrieval  (default: {0})
+            skip_step {int} -- offset for data paging, range between 0 - 30 (default: {30})
+            sleep_ {bool} -- whether to pause for 0.5 secs between each request
+            (used only, when method get_all_data() is called, since it is using multiprocessing) (default: {False})
+        
+        Returns:
+            Any -- pandas DataFrame in self.data attribute
+        """
+
+        data = None
 
         while True:
             filter_ = '?filter={"skip": %d}' % (skip_start)
@@ -91,14 +112,19 @@ class API:
 
         return self
 
-    def worker(self, api_suffix: Text, sleep_=True) -> Any:
-        self._get_all(api_suffix, sleep_=sleep_).save_data(f"{api_suffix}"[:30])
-
-    def get_all_data(self) -> None:
-        with Pool(maxtasksperchild=1) as p:
-            p.map(self.worker, list(API_SUFFIXES.values()))
-
     def _get_single(self, api_suffix: Text, id_: Text) -> Any:
+        """Private method.
+        Gets data for provided id_ parameter from given resource of the api, as specified via api_suffix
+        argument.
+        Data are stored as pandas dataframe in public self.data attribute of the API class instance.
+        
+        Arguments:
+            api_suffix {Text} -- resource for specific data which is added to base api url
+            id_ {Text} -- unique id of the dataset which is accessed via api resource using api_suffix argument
+        
+        Returns:
+            Any -- pandas DataFrames stored id self.data attribute
+        """
         reurl = f"{self._api_url_base}{api_suffix}/{id_}"
         response = r.get(reurl, headers=self._api_auth_header)
         dj = response.json()
@@ -118,35 +144,108 @@ class API:
         return self
 
     def _create_dirs_if_not_exist(self, dirpath: Text) -> None:
+        """Private method.
+        Checks, if directory in given directory path exists.
+        If not, creates all directories recursively on provided path.
+        
+        Arguments:
+            dirpath {Text} -- absolute path to the directory which is to be checked
+        """
         if not isdir(dirpath):
             makedirs(dirpath, exist_ok=True)
 
     def replace_labels(self, labels: Dict) -> Any:
+        """Replaces labels in pandas DataFrame object.
+        
+        Arguments:
+            labels {Dict} -- labels which are to be used as replacement
+        
+        Returns:
+            Any -- pandas DataFrame object with replaced labels
+        """
         try:
             self.data = self.data.rename(columns=labels)
             return self
         except Exception:
             return self
 
+    def _worker(self, api_suffix: Text, sleep_=True) -> Any:
+        """Private method.
+        Worker for multiprocessed method "get_all_data".
+        Gets data from given api resource into the pandas DataFrame object.
+        
+        Arguments:
+            api_suffix {Text} -- resource for specific data which is added to base api url
+        
+        Keyword Arguments:
+            sleep_ {bool} -- whether to pause for 0.5 secs between each request (default: {True})
+        
+        Returns:
+            Any -- pandas DataFrame object stored in self.data attribute
+        """
+        self._get_all(api_suffix, sleep_=sleep_).save_data(f"{api_suffix}"[:30])
+
+    def get_all_data(self) -> None:
+        """Gets data from all resources of the API.
+        Resources are specified in API_SUFFIXES dict.
+        """
+        with Pool(maxtasksperchild=1) as p:
+            p.map(self._worker, list(API_SUFFIXES.values()))
+
     def get_all_lide_domy_byty(self, human_labels=False) -> Any:
+        """Gets all data from api resource "lide-domy-byty".
+        
+        Keyword Arguments:
+            human_labels {bool} -- whether to change default "code" labels to human readeble ones (default: {False})
+        
+        Returns:
+            Any -- pandas DataFrame object stored in self.data attribute
+        """
         self._get_all(API_SUFFIXES["L_D_B"])
         if human_labels:
             self.replace_labels(LABELS_LIDE_DOMY_BYTY)
         return self
 
     def get_single_lide_domy_byty(self, id_: Text, human_labels=False) -> Any:
+        """Gets data for given id from specific api resource "lide-domy-byty".
+        
+        Arguments:
+            id_ {Text} -- unique id of the data of the api resource
+        
+        Keyword Arguments:
+            human_labels {bool} -- whether to change default "code" labels to human readeble ones (default: {False})
+        
+        Returns:
+            Any -- pandas DataFrame object stored in self.data attribute
+        """
         self._get_single(API_SUFFIXES["L_D_B"], id_)
         if human_labels:
             self.replace_labels(LABELS_LIDE_DOMY_BYTY)
         return self
 
     def get_all_vyjizdky_do_zamestnani(self, human_labels=False) -> Any:
+        """Gets all data from api resource "vyjizdky-zamestnani"
+        
+        Keyword Arguments:
+            human_labels {bool} -- whether to change default "code" labels to human readeble ones (default: {False})
+        
+        Returns:
+            Any -- pandas DataFrame object stored in self.data attribute
+        """
         self._get_all(API_SUFFIXES["V_Z"])
         if human_labels:
             self.replace_labels(LABELS_VYJIZDKY_ZAMESTNANI)
         return self
 
     def get_all_ceny_potravin(self, human_labels=False) -> Any:
+        """Gets all data from api resource "prumerne-spotrebitelske-ceny-vybranych-vyrobku-potravinarske-vyrobky"
+        
+        Keyword Arguments:
+            human_labels {bool} -- whether to change default "code" labels to human readeble ones (default: {False})
+        
+        Returns:
+            Any -- pandas DataFrame object stored in self.data attribute
+        """
         self._get_all(API_SUFFIXES["P_C_P_V"])
         if human_labels:
             # TODO
@@ -154,6 +253,16 @@ class API:
         return self
 
     def save_data(self, filename: Text, dirpath=DATA_OUTPUT) -> None:
+        """Saves data in pandas DataFrame object into xlsx file.
+        Filename and sheetname are specified by filename argument.
+        !!!Sheetname can be at max 31 chars long!!!
+        
+        Arguments:
+            filename {Text} -- string used to name file and data sheet
+        
+        Keyword Arguments:
+            dirpath {[type]} -- absolute path to directory, where file is saved (default: {DATA_OUTPUT})
+        """
         print("Saving data...")
         self._create_dirs_if_not_exist(dirpath)
         filepath = join(dirpath, f"{filename}.xlsx")
